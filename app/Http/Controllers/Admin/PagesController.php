c4b1cd65c670cf53db09ce8e2b9ec6072ff72373
SongTranVan <songtranvan2511@gmail.com>
2020-05-18 16:03:51 +0700

    finish show newest post by category

--- app/Http/Controllers/Admin/PagesController.php
+++ app/Http/Controllers/Admin/PagesController.php
@@ -3,12 +3,51 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Carbon;
 
 class PagesController extends Controller
 {
     public function home()
     {
-        return view('layouts.admin_main');
+        $users = User::select('id', 'created_at')
+            ->get()
+            ->groupBy(function ($date)
+            {
+                return Carbon::parse($date->created_at)->format('m');
+            });
+
+        $usermcount = [];
+        $userArr = [];
+
+        foreach($users as $key => $value)
+        {
+            $usermcount[(int)$key] = count($value);
+        }
+
+        for ($i = 1; $i <= 12; $i++)
+        {
+            if (!empty($usermcount[$i]))
+            {
+                $userArr[$i] = $usermcount[$i];
+            }
+            else
+            {
+                $userArr[$i] = 0;
+            }
+        }
+
+        $page_views = Post::select('page_views')->get();
+        $total_page_views = 0;
+
+        foreach($page_views as $value)
+        {
+            $total_page_views += $value->page_views;
+        }
+
+        $total_posts = Post::count();
+        $total_users = User::count();
+        return view('manager.static_pages.dashboard', compact('total_posts', 'total_users', 'total_page_views'));
     }
 }
--- resources/views/shared/admin_sidebar.blade.php
+++ resources/views/shared/admin_sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ route('app.home') }}" class="brand-link">
+    <a href="{{ route('manager.admin_dashboard') }}" class="brand-link">
         <img src="{!! asset('img/core-img/logo.png') !!}" alt="Vizew Logo" class="brand-image elevation-3">
         <span class="brand-text font-weight-light">Dashboard</span>
     </a>
