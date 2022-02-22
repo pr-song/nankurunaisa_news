ab0867ff5bb43656cdabfa12f5511eab291e4ba7
pr-song <34098658+pr-song@users.noreply.github.com>
2022-02-22 20:22:39 +0900

    Revert "added hot news and hot users function"

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Category;
-use App\Models\User;
 
 class StaticPagesController extends Controller
 {
@@ -18,15 +17,8 @@ class StaticPagesController extends Controller
         $trending_posts = Post::orderBy('page_views', 'desc')->limit(3)->get();
         $lated_posts = Post::orderBy('created_at', 'desc')->limit(5)->get();
         $post = Post::whereDate('created_at', now()->startOfMonth())->get();
-        $featured_posts = Post::orderBy('likes_count', 'desc')->limit(3)->get();
-        $hot_users = User::join('posts', 'users.id', '=', 'posts.user_id')
-            ->select('users.name', Post::raw('count(posts.user_id) as posts_count'))
-            ->groupBy('posts.user_id')
-            ->orderByDesc(Post::raw('count(posts.user_id)'), 'desc')
-            ->limit(5)
-            ->get();
 
-        return view('home', compact('trending_posts', 'lated_posts', 'sport', 'business', 'travel', 'education', 'game', 'featured_posts','hot_users'));
+        return view('home', compact('trending_posts', 'lated_posts', 'sport', 'business', 'travel', 'education', 'game'));
     }
 
     public function about()
